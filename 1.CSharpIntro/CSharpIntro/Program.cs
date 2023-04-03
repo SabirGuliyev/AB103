fd1ffe1cde962b5cd00120f9789bf8cb8df75ae4
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-04-03 16:04:15 +0400

    C# intro added

---
+++ 1.CSharpIntro/CSharpIntro/Program.cs
@@ -0,0 +1,86 @@
+﻿
+
+byte age = 18;
+//string gender = "female";
+
+
+
+
+
+
+//if (age >= 18 && gender=="female")
+//{
+
+//    if (gender=="female")
+//    {
+//        Console.WriteLine("Ishe qebul olduz");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Xanim ishci axtarilir");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Get boyuyende gelersen");
+//}
+
+//byte choice = 2;
+//switch (choice)
+//{
+//    case 1:
+//        Console.WriteLine("Az dili secildi");
+//        break;
+//    case 2 or 3:
+//        Console.WriteLine("Diger diller secildi");
+//        break;
+
+//    default:
+//        Console.WriteLine("Yalnish secim");
+//        break;
+//}
+
+
+
+//for (int i = 0; i < 10; i++)
+//{
+    
+//    Console.WriteLine(i);
+//}
+
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+//int a = 0;
+//int b;
+//while (true)
+//{
+//    a++;
+//    b= 0;
+//    while (true)
+//    {
+//        a--;
+//        b++;
+//        int c = 0;  
+//    }
+//}
+
+bool result = false;
+
+//while (result)
+//{
+//    Console.WriteLine("while ishledi");
+//}
+
+string gender;
+do
+{
+    Console.WriteLine("Cinsi secin");
+    gender=Console.ReadLine();
+
+}while (gender!="male");
