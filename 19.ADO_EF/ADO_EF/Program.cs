170f48c85abc31f8e5e48c24517f65bf19b79761
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-05-05 17:29:24 +0400

    ADO.NET EF added

---
+++ 19.ADO_EF/ADO_EF/Program.cs
@@ -0,0 +1,91 @@
+﻿using ADO_EF.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO_EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region ADO.NET
+            //Student student=new Student
+            //{
+            //    Name="Cavidan",
+            //    Point=99
+            //};
+
+            //int result;
+            //using (SqlConnection connection = new SqlConnection("Server=MSI;Database=AB103Joins;Trusted_connection=true;Integrated security=true;"))
+            //{
+            //    connection.Open();
+            //    string cmd = $"INSERT INTO Students VALUES('{student.Name}',{student.Point},2)";
+            //    //string cmd = "DELETE FROM Students WHERE Id=13";
+            //    SqlCommand command = new SqlCommand(cmd, connection);
+            //    result = command.ExecuteNonQuery();
+            //}
+
+
+
+
+
+
+            //    if (result>0)
+            //{
+            //    Console.WriteLine("Student successfully DELETED");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error occured");
+            //}
+
+
+            //string query = "SELECT * FROM Students";
+            //DataTable table=new DataTable();
+            //using (SqlConnection connection = new SqlConnection("Server=MSI;Database=AB103Joins;Trusted_connection=true;Integrated security=true;"))
+            //{
+            //    //SqlCommand cmd = new SqlCommand(query,connection);
+            //    //cmd.ExecuteScalar();
+
+            //    connection.Open();
+
+            //    SqlDataAdapter adapter = new SqlDataAdapter(query,connection);
+            //    adapter.Fill(table);
+            //}  
+
+
+            //List<Student> students = new List<Student>();
+
+
+            //foreach (DataRow item in table.Rows)
+            //{
+            //    //Console.WriteLine(+" "++" " + item["point"]);
+
+            //    Student student2 = new Student
+            //    {
+            //        Id=Convert.ToInt32(item["id"]),
+            //        Name=item["name"].ToString(),
+            //        Point=Convert.ToInt32(item["point"])
+            //    };
+            //    students.Add(student2);
+
+            //}
+
+
+
+            //foreach (var item in students)
+            //{
+            //    Console.WriteLine(item.Name+" "+item.Point);
+            //} 
+            #endregion
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 20.EF_Step/EF_Step/DAL/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EF_Step.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Step.DAL
+{
+    internal class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("server=msi;database=EFCoreAB103;trusted_connection=true;integrated security=true;encrypt=false;");
+        }
+
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
