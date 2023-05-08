ca9353b3489a1df8aa16d757bd14ce038271afd7
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-05-08 16:52:05 +0400

    MVC added

---
+++ 21.MVCSTart/MVCSTart/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCSTart.Models;
+
+namespace MVCSTart.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+         
+
+            TempData["Students"]="Tempden gonderilen data";
+
+            return RedirectToAction("About");
+        }
+        public IActionResult About()
+        {
+            
+
+            return View();
+        }
+
+    }
+}
---
+++ 21.MVCSTart/MVCSTart/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name:"Default",
+    pattern:"{controller=home}/{action=index}/{id?}"
+    );
+
+app.Run();
